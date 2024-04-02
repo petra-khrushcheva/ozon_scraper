b59e7769b7987988e330f9cd186e685988e7f242
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-04-02 12:35:16 +0500

    add db models

--- src/products/models.py
+++ src/products/models.py
@@ -1,8 +1,5 @@
-import enum
-from uuid import UUID
-
-from sqlalchemy import ForeignKey, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, String, Text, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core import Base
 
@@ -10,51 +7,36 @@ from src.core import Base
 class Product(Base):
     __tablename__ = "products"
 
+    id: Mapped[int]
     name: Mapped[str] = mapped_column(String(50))
     description: Mapped[str | None] = mapped_column(Text)
-
-#где то здесь должна быть запись даты или номера парсинга
-
-
-# class Workspace(Base):
-#     __tablename__ = "workspaces"
-
-#     name: Mapped[str] = mapped_column(String(50))
-#     description: Mapped[str | None] = mapped_column(Text)
-
-#     tasks: Mapped[list["Task"]] = relationship(  # noqa: F821
-#         back_populates="workspace",
-#         primaryjoin=(
-#             "and_(Workspace.id==Task.workspace_id, Task.is_active==True)"
-#         ),
-#         order_by="Task.created_at.desc()",
-#     )
+    image_url: Mapped[str]
 
 
-# class WorkspaceUserAssociation(Base):
-#     __tablename__ = "workspace_user_association"
-#     __table_args__ = (
-#         UniqueConstraint(
-#             "user_id",
-#             "workspace_id",
-#             name="unique_workspace_user",
-#         ),
-#     )
+class ScrapingEvent(Base):
+    __tablename__ = "scraping_events"
 
-#     workspace_id: Mapped[UUID] = mapped_column(
-#         ForeignKey("workspaces.id", ondelete="CASCADE"), nullable=False
-#     )
-#     user_id: Mapped[UUID] = mapped_column(
-#         ForeignKey("users.id", ondelete="CASCADE"), nullable=False
-#     )
-#     user_role: Mapped[GroupRole] = mapped_column(
-#         default=GroupRole.user.value, server_default=GroupRole.user.value
-#     )
+    id: Mapped[int] = mapped_column(primary_key=True)
+    products_count: Mapped[int]
 
-#     workspace: Mapped["Workspace"] = relationship(
-#         back_populates="users",
-#     )
 
-#     user: Mapped["User"] = relationship(  # noqa: F821
-#         back_populates="workspaces",
-#     )
+class ProductScrapingAssociation(Base):
+    __tablename__ = "product_scraping_association"
+    __table_args__ = (
+        UniqueConstraint(
+            "product_id",
+            "scraping_id",
+            name="unique_product_scraping",
+        ),
+    )
+
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey("products.id", ondelete="CASCADE"), nullable=False
+    )
+    scraping_id: Mapped[int] = mapped_column(
+        ForeignKey("scraping_events.id", ondelete="CASCADE"), nullable=False
+    )
+    price: Mapped[int]
+    discount: Mapped[str | None]
+
+    product: Mapped["Product"] = relationship()
