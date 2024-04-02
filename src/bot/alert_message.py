6b800698eab690978cf1745e8d608d649a36eea1
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-04-02 16:10:20 +0500

    update bot commands

--- src/bot/alert_message.py
+++ src/bot/alert_message.py
@@ -1,7 +1,12 @@
 from aiogram import Bot
 
+from src.bot import bot
 from src.core import settings
 
 
-async def send_alert(bot: Bot, count: int = 10):
-    bot.send_message(chat_id=settings.chat_id, text="")
+async def send_alert(products_count: int, bot: Bot = bot):
+    bot.send_message(
+        chat_id=settings.chat_id,
+        text=("Задача на парсинг товаров с сайта Ozon завершена. "
+              f"Сохранено: {products_count} товаров")
+    )
--- src/bot/router.py
+++ src/bot/router.py
@@ -1,8 +1,38 @@
 from aiogram import Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold, hlink
+
+from src.core import async_session
+from src.products import get_last_scraping_products
 
 router = Router()
 
 
-@router.message()
-async def product_list_handler():
-    pass
+@router.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(
+        text="Этот бот присылает вам уведомления о завершении парсинга товаров"
+        " с сайта Ozon. Вы можете посмотреть результаты последнего парсинга по"
+        " команде /Список_товаров"
+    )
+
+
+@router.message(Command("Список_товаров"))
+async def product_list_handler(message: Message):
+    with async_session() as session:
+        products = await get_last_scraping_products(session=session)
+    if products is not None:
+        text = ""
+        number = 1
+        for product in products:
+            number = 1
+            product_link = hlink(
+                title=products.name,
+                url=f'https://www.ozon.ru/product/{product.slug}'
+            )
+            line = f"{hbold(number)}. {product_link}\n"
+            text += line
+            number += 1
+        message.answer(text=text)
+    message.answer(text="Парсинга еще не было")
