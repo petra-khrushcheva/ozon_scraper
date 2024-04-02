cf9251e3d4522c5542ad6de1b9f8de6f03ac2364
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-04-02 13:08:50 +0500

    update app router

--- src/products/router.py
+++ src/products/router.py
@@ -1,15 +1,13 @@
 from typing import List
-from uuid import UUID
 
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src import scraper
 from src.core import get_session
 from src.products import services
-
 # from src.products.dependencies import ...
 from src.products.schemas import Product, ProductsCount
-from src import scraper
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
@@ -20,26 +18,26 @@ async def start_scraping(
     session: AsyncSession = Depends(get_session),
 ):
     """
-    POST /v1/products/: Запуск задачи на парсинг N товаров. Количество товаров
-    должно приниматься в теле запроса в параметре products_count, по умолчанию
+    Запуск задачи на парсинг N товаров. Количество товаров
+    принимается в теле запроса в параметре products_count, по умолчанию
     10 (если значение не было передано), максимум 50.
     """
-    return await scraper.start_scraping()
+    return await scraper.start_scraping(products_count=input)
 
 
 @router.get("/", response_model=List[Product])
 async def get_products(session: AsyncSession = Depends(get_session)):
     """
-    • GET /v1/products/: Получение списка товаров.
+    Получение списка товаров последнего парсинга.
     """
-    return await services.get_products(session=session)
+    return await services.get_last_scraping_products(session=session)
 
 
 @router.get("/{product_id}", response_model=Product)
 async def get_product(
-    product_id: UUID, session: AsyncSession = Depends(get_session)
+    product_id: int, session: AsyncSession = Depends(get_session)
 ):
     """
-    • GET /v1/products/{product_id}/: Получение товара по айди.
+    Получение товара по айди.
     """
     return await services.get_product(session=session)
--- src/products/services.py
+++ src/products/services.py
@@ -4,11 +4,11 @@ from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.products.models import Product
+from src.products.models import Product, ProductScrapingAssociation
 from src.products.schemas import Product, ProductsCount
 
 
-async def get_products(session: AsyncSession):
+async def get_last_scraping_products(session: AsyncSession):
     pass
 
 
