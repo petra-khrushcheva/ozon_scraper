d1bab8d068e2bbd6ea804d10e7ef480598a3419c
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-04-04 00:06:30 +0500

    add docstrings to bot router functions

--- src/bot/router.py
+++ src/bot/router.py
@@ -10,28 +10,35 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def command_start_handler(message: Message):  # а так тексты переносить вообще можно???
+async def command_start_handler(message: Message):
+    """Обработчик команды /start"""
     await message.answer(
-        text="Этот бот присылает вам уведомления о завершении парсинга товаров"
-        " с сайта Ozon. Вы можете посмотреть результаты последнего парсинга по"
-        " команде /Список_товаров"
+        text=(
+            "Этот бот присылает вам уведомления о завершении парсинга товаров "
+            "с сайта Ozon. Вы можете посмотреть результаты последнего парсинга"
+            " по команде /products_list")
     )
 
 
-@router.message(Command("Список_товаров"))
+@router.message(Command("products_list"))
 async def product_list_handler(message: Message):
-    with async_session() as session:
+    """
+    Обработчик команды /products_list.
+    Присылает список товаров последнего парсинга.
+    """
+    async with async_session() as session:
         products = await get_last_scraping_products(session=session)
-    if products is not None:
-        text = ""
-        number = 1
-        for product in products:
-            product_link = hlink(
-                title=product.name,
-                url=f'https://www.ozon.ru/product/{product.slug}'
-            )
-            line = f"{hbold(number)}. {product_link}\n"
-            text += line
-            number += 1
-        message.answer(text=text)
-    message.answer(text="Парсинга еще не было")
+    if not products:
+        await message.answer(text="Парсинга еще не было")
+        return
+    text = ""
+    number = 1
+    for product in products:
+        product_link = hlink(
+            title=product.name,
+            url=f'https://www.ozon.ru/product/{product.slug}'
+        )
+        line = f"{hbold(number)}. {product_link}\n"
+        text += line
+        number += 1
+    await message.answer(text=text)
