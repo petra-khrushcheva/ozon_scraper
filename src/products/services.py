e7aa4985ecd9cb2832d24d7a203f401bcb9b8fdc
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-04-04 23:04:44 +0500

    linter

--- src/products/services.py
+++ src/products/services.py
@@ -30,7 +30,7 @@ async def get_last_scraping_products(session: AsyncSession):
         )
         result: Result = await session.execute(stmt)
         return result.unique().all()
-    return
+    return None
 
 
 async def get_product(product_id: UUID, session: AsyncSession):
