ca3afa87bc1413a18a28c6b60ccd83a9f47f5f98
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-04-04 16:06:14 +0500

    fix get product service

--- src/products/services.py
+++ src/products/services.py
@@ -1,35 +1,59 @@
 from uuid import UUID
 
-from sqlalchemy import desc, select
+from sqlalchemy import and_, desc, func, select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
 
 from src.products.models import (Product, ProductScrapingAssociation,
                                  ScrapingEvent)
 
 
 async def get_last_scraping_products(session: AsyncSession):
-    last_scraping = (await session.execute(
-        select(ScrapingEvent).order_by(desc(ScrapingEvent.id)).limit(1)
-    )).scalar_one_or_none()
+    """Получение списка товаров последнего парсинга"""
+    last_scraping = (
+        await session.execute(
+            select(ScrapingEvent).order_by(desc(ScrapingEvent.id)).limit(1)
+        )
+    ).scalar_one_or_none()
     if last_scraping is not None:
         stmt = (
             select(
                 ProductScrapingAssociation.discount,
                 ProductScrapingAssociation.price,
                 Product.name,
-                Product.description,
                 Product.image_url,
-                Product.slug
+                Product.slug,
             )
+            .select_from(ProductScrapingAssociation)
+            .join(ProductScrapingAssociation.product)
             .where(ProductScrapingAssociation.scraping_id == last_scraping.id)
-            .options(joinedload(ProductScrapingAssociation.product))
         )
         result: Result = await session.execute(stmt)
-        return result.unique().scalars().all()
+        return result.unique().all()
 
 
 async def get_product(product_id: UUID, session: AsyncSession):
+    """Получение одного товара и его последней цены"""
     result: Result = await session.get(Product, product_id)
-    return result.scalar_one_or_none()
+    subq = (
+        select(func.max(ProductScrapingAssociation.scraping_id))
+        .where(ProductScrapingAssociation.product_id == product_id)
+        .scalar_subquery()
+    )
+    stmt = (
+        select(
+            ProductScrapingAssociation.discount,
+            ProductScrapingAssociation.price,
+            Product.name,
+            Product.image_url,
+            Product.slug,
+        )
+        .select_from(ProductScrapingAssociation)
+        .join(ProductScrapingAssociation.product)
+        .where(
+            and_(ProductScrapingAssociation.scraping_id == subq),
+            ProductScrapingAssociation.product_id == product_id,
+        )
+    )
+    result: Result = await session.execute(stmt)
+    return result.one_or_none()
